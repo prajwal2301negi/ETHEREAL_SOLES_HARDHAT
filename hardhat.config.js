e0d9233694bbaab35d22da71aef9de3034407240
Prajwal Negi <166615651+prajwalnegi2301@users.noreply.github.com>
2024-07-16 23:25:21 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.4",
+// };
+
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  }
+
+};
